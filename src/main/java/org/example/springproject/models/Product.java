9cb302fb2268953f305e7f70b972ff87e9864584
inesEngel <ines.engel@stud.ubbcluj.ro>
2025-10-05 20:52:29 +0300

    project setup

--- src/main/java/org/example/springproject/models/Product.java
+++ src/main/java/org/example/springproject/models/Product.java
@@ -1,4 +1,19 @@
 package org.example.springproject.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer product_id;
+
+    private String name;
+    private String category;
+    private String size;
+    private String color;
+    private Double price;
+    private Integer stock;
+    private Integer version;
 }
